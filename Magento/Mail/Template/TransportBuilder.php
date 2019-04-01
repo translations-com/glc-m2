54aaac3a8cfd5aba86a58117129a610579e118e2
Saul Rotblatt <srotblatt@translations.com>
2019-04-01 11:46:52 -0600

    1.1.0_RC2 Part B

--- Magento/Mail/Template/TransportBuilder.php
+++ Magento/Mail/Template/TransportBuilder.php
@@ -3,18 +3,19 @@ namespace TransPerfect\GlobalLink\Magento\Mail\Template;
 
 class TransportBuilder extends \Magento\Framework\Mail\Template\TransportBuilder
 {
-    public function addAttachment(
-        $body,
-        $mimeType = \Zend_Mime::TYPE_OCTETSTREAM,
-        $disposition = \Zend_Mime::DISPOSITION_ATTACHMENT,
-        $encoding = \Zend_Mime::ENCODING_BASE64,
-        $filename = null
-    ) {
-        $attachment = new \Zend\Mime\Part($body);
-        $attachment->type = $mimeType;
-        $attachment->disposition = $disposition;
-        $attachment->encoding = $encoding;
-        $attachment->filename = $filename;
-        return $attachment;
+    protected $message;
+
+    public function addAttachment($body, $filename = null)
+    {
+        $mimeType = \Zend_Mime::TYPE_OCTETSTREAM;
+        $this->message->setBodyAttachment($body, $filename, $mimeType);
+        return $this;
+    }
+
+    public function prepareMessage()
+    {
+        parent::prepareMessage();
+        $this->message->setPartsToBody();
+        return $this;
     }
 }
