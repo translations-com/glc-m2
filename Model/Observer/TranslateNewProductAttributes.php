57c4e682f9101b46d582aaeea37f2de6f7eb3183
Justin Griffin <jgriffin@translations.com>
2022-11-15 10:14:56 -0700

    Update to RC1 Adding More Entity Trigger Support

--- Model/Observer/TranslateNewProductAttributes.php
+++ Model/Observer/TranslateNewProductAttributes.php
@@ -12,7 +12,7 @@ namespace TransPerfect\GlobalLink\Model\Observer;
 use TransPerfect\GlobalLink\Helper\Data;
 use TransPerfect\GlobalLink\Helper\Ui\Logger;
 
-class TranslateNewProducts implements \Magento\Framework\Event\ObserverInterface
+class TranslateNewAttributes implements \Magento\Framework\Event\ObserverInterface
 {
     protected $logger;
     protected $productRepository;
@@ -48,35 +48,35 @@ class TranslateNewProducts implements \Magento\Framework\Event\ObserverInterface
 
     public function execute(\Magento\Framework\Event\Observer $observer)
     {
-        $useAutomation = $this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_products/translate_new_products', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-        $product = $observer->getEvent()->getProduct();
-        if ($product->isObjectNew() && $useAutomation) {
+        $useAutomation = $this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_product_attributes/translate_new_product_attributes', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        $attribute = $observer->getEvent()->getAttribute();
+        if ($attribute->isObjectNew() && $useAutomation) {
 
             $stores = $this->storeRepository->getList();
             $targetStores = [];
-            $sourceLocale = $this->helper->getStoreIdFromLocale($this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_products/tnp_source_locale', \Magento\Store\Model\ScopeInterface::SCOPE_STORE))[0];
+            $sourceLocale = $this->helper->getStoreIdFromLocale($this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_product_attributes/tnpa_source_locale', \Magento\Store\Model\ScopeInterface::SCOPE_STORE))[0];
             foreach ($stores as $store) {
                 if ($store->getData("store_id") != $sourceLocale && $store->getData('locale') != '') {
                     $targetStores[] = $store->getId();
                 }
             }
-            $shortCode = $this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_products/tnp_project', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-            $confirmationEmail = $this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_products/tnp_confirmation_email', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-            $includeOptions = $this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_products/tnp_include_options', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
-            $numberOfDays = $this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_products/tnp_number_of_days', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+            $shortCode = $this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_product_attributes/tnpa_project', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+            $confirmationEmail = $this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_product_attributes/tnpa_confirmation_email', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+            $includeOptions = $this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_product_attributes/tnpa_include_options', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+            $numberOfDays = $this->scopeConfig->getValue('globallink_translate_new_entities/translate_new_product_attributes/tnpa_number_of_days', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
             $queue = $this->queueFactory->create();
             $queueData = [
-                'name' => 'Auto Submission: ' .  $product->getName(),
+                'name' => 'Auto Submission: ' .  $attribute->getName(),
                 'submission_instructions' => '',
                 'project_shortcode' => $shortCode,
-                'entity_type_id' => $this->helper::CATALOG_PRODUCT_TYPE_ID,
+                'entity_type_id' => $this->helper::PRODUCT_ATTRIBUTE_TYPE_ID,
                 'magento_admin_user_requested_by' => $this->_auth->getUser()->getId(),
                 'request_date' => $this->_dateTime->gmtTimestamp(),
                 'due_date' => ($this->_dateTime->gmtTimestamp() + (24*60*60*$numberOfDays) - 1),
                 'include_associated_and_parent_categories' => 0,
                 'priority' => "0",
                 'origin_store_id' => $sourceLocale,
-                'items' => [$product->getId() => $product->getName()],
+                'items' => [$attribute->getId() => $attribute->getName()],
                 'localizations' => $targetStores,
                 'confirmation_email' => $confirmationEmail,
                 'refresh_nontranslatable_fields' => 0,
@@ -85,14 +85,14 @@ class TranslateNewProducts implements \Magento\Framework\Event\ObserverInterface
             $queue->setData($queueData);
             try {
                 $queue->getResource()->save($queue);
-                $this->messageManager->addSuccessMessage(__('Product has been saved to the translate queue'));
+                $this->messageManager->addSuccessMessage(__('Product attribute has been saved to the translate queue'));
                 if ($this->logger->isDebugEnabled()) {
-                    $this->logger->logAction(Data::CATALOG_PRODUCT_TYPE_ID, Logger::SEND_ACTION_TYPE, $queueData);
+                    $this->logger->logAction(Data::PRODUCT_ATTRIBUTE_TYPE_ID, Logger::SEND_ACTION_TYPE, $queueData);
                 }
             } catch (\Exception $e) {
                 $this->messageManager->addErrorMessage($e->getMessage());
                 if ($this->logger->isErrorEnabled()) {
-                    $this->logger->logAction(Data::CATALOG_PRODUCT_TYPE_ID, Logger::SEND_ACTION_TYPE, $queueData, Logger::CRITICAL, $e->getMessage());
+                    $this->logger->logAction(Data::PRODUCT_ATTRIBUTE_TYPE_ID, Logger::SEND_ACTION_TYPE, $queueData, Logger::CRITICAL, $e->getMessage());
                 }
             }
         }
