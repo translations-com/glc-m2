bff9fc001a2f703c5d788c44476c853307fe6347
Justin Griffin <jgriffin@translations.com>
2022-11-29 13:36:42 -0700

    Update Project.php
    More graceful error handling for new entity automation properties page.

--- Model/Config/Source/Project.php
+++ Model/Config/Source/Project.php
@@ -22,15 +22,21 @@ class Project implements \Magento\Framework\Option\ArrayInterface
     public function toOptionArray()
     {
         $projectOptions = [];
-        $shortCodes = array_map('trim', explode(",", $this->scopeConfig->getValue('globallink/general/project_short_codes', \Magento\Store\Model\ScopeInterface::SCOPE_STORE)));
-        if (count($shortCodes) == 0) {
-            $projectOptions[0] = ['value' => 0, 'label' => 'No project short codes have been entered above'];
-            return $projectOptions;
-        } else {
-            foreach($shortCodes as $shortCode){
-                $projectOptions[] = ['value' => $shortCode, 'label' => $shortCode];
+        $shortCodeProperty = $this->scopeConfig->getValue('globallink/general/project_short_codes', \Magento\Store\Model\ScopeInterface::SCOPE_STORE);
+        if(!empty($shortCodeProperty)){
+            $shortCodes = array_map('trim', explode(",", $shortCodeProperty));
+            if (count($shortCodes) == 0) {
+                $projectOptions[0] = ['value' => 0, 'label' => 'No project short codes have been configured in properties.'];
+                return $projectOptions;
+            } else {
+                foreach($shortCodes as $shortCode){
+                    $projectOptions[] = ['value' => $shortCode, 'label' => $shortCode];
+                }
             }
+        } else{
+            $projectOptions[0] = ['value' => 0, 'label' => 'No project short codes have been configured in properties.'];
         }
+
         return $projectOptions;
     }
 }
